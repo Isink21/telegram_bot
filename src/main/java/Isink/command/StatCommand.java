1ec12eac58b6629abe3d14ce5f475f58ba423dd2
Iliya Sinkevich <iliya.sinkevich@timvero.com>
2022-06-26 19:01:41 +0300

    JRTB-10: fix

--- src/main/java/Isink/command/StatCommand.java
+++ src/main/java/Isink/command/StatCommand.java
@@ -2,6 +2,7 @@ package Isink.command;
 
 import Isink.SendBotMessageService;
 import Isink.user.service.TelegramUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class StatCommand implements Command {
@@ -11,6 +12,7 @@ public class StatCommand implements Command {
 
     public final static String STAT_MESSAGE = "Isink Telegram Bot использует %s человек.";
 
+    @Autowired
     public StatCommand(SendBotMessageService sendBotMessageService, TelegramUserService telegramUserService) {
         this.sendBotMessageService = sendBotMessageService;
         this.telegramUserService = telegramUserService;
